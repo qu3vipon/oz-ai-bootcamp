41d431dc9c3cbce99aacabb0f6ebbd5f36d91114
qu3vipon <sdh5813@gmail.com>
2026-01-23 16:43:01 +0900

    260123

--- docker/api/main.py
+++ docker/api/main.py
@@ -1,14 +1,49 @@
-from fastapi import FastAPI
-from sqlalchemy import text
+import json
+import uuid
 
-from connection import SessionFactory
+from fastapi import FastAPI, Body
+from fastapi.responses import StreamingResponse
+from redis import asyncio as aredis
 
+
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True)
 
 app = FastAPI()
 
-@app.get("/health")
-def health_check_api():
-    with SessionFactory() as session:
-        stmt = text("SELECT name FROM test LIMIT 1")
-        result = session.execute(stmt).fetchone()
-    return {"ping": "pong"}
+@app.post("/generate")
+async def generate_api(user_input: str = Body(...)):
+    # job_id 생성
+    job_id = str(uuid.uuid4())
+    
+    # 결과 채널 구독
+    channel = f"result:{job_id}"
+
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
+    print(f"구독 시작: {job_id}")
+
+    # Enqueue(LPUSH)
+    job = {"id": job_id, "input": user_input}
+    await redis_client.lpush("inference_queue", json.dumps(job))
+    print(f"큐에 작업 추가: {job}")
+
+    # 결과를 돌려받아서 응답
+    async def event_generator():
+        print("Listening 시작...")
+        async for message in pubsub.listen():
+            if message["type"] != "message":
+                continue
+
+            data = message["data"]
+            if data == "[DONE]":
+                break
+            yield data
+        
+        await pubsub.unsubscribe(channel)
+        await pubsub.close()
+        print("Listening 종료...")
+
+    return StreamingResponse(
+        event_generator(),
+        media_type="text/event-stream"
+    )
--- docker/worker/main.py
+++ docker/worker/main.py
@@ -1,1 +1,67 @@
-print("WORKER")
+import json
+import redis
+
+from llama_cpp import Llama
+
+
+redis_client = redis.from_url("redis://redis:6379", decode_responses=True)
+
+llm = Llama(
+    model_path="./models/Llama-3.2-1B-Instruct-Q4_K_M.gguf",
+    n_ctx=4096,
+    n_threads=2,
+    verbose=False,
+    chat_format="llama-3",
+)
+
+SYSTEM_PROMPT = (
+    "You are a concise assistant. "
+    "Always reply in the same language as the user's input. "
+    "Do not change the language. "
+    "Do not mix languages."
+)
+
+def inference_and_publish(channel, user_input):
+    print("추론 시작...")
+    response = llm.create_chat_completion(
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": user_input},
+        ],
+        max_tokens=256,
+        temperature=0.7,
+        stream=True,
+    )
+
+    for chunk in response:
+        token = chunk["choices"][0]["delta"].get("content")
+        if token:
+            redis_client.publish(channel, token)
+    
+    redis_client.publish(channel, "[DONE]")
+    print("추론 종료...")
+
+
+def run():
+    while True:
+        # Queue를 확인해서, Job(job_id, input) 꺼내기
+        _, job_data = redis_client.brpop("inference_queue")
+        
+        job = json.loads(job_data)
+
+        job_id = job["id"]
+        user_input = job["input"]
+
+        channel = f"result:{job_id}"
+
+        # 추론 & publish
+        try:
+            inference_and_publish(channel=channel, user_input=user_input)
+        except Exception as e:
+            print(f"[Error] {str(e)}")
+            redis_client.publish(channel, "[DONE]")
+
+
+# 이 파일을 직접한 실행한 경우에만, run() 호출
+if __name__ == "__main__":
+    run()
