c72298598ccf558dff1ebe290f19e6d0d0dd7359
Dong-hyeon Shin <52447545+qu3vipon@users.noreply.github.com>
2026-03-12 14:49:26 +0900

    260312

---
+++ risk_predict/database/connection.py
@@ -0,0 +1,27 @@
+from sqlalchemy import event
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
+DATABASE_URL = "sqlite+aiosqlite:///./db.sqlite"
+
+engine = create_async_engine(DATABASE_URL)
+
+@event.listens_for(engine.sync_engine, "connect")
+def enable_sqlite_fk(dbapi_connection, connection_record):
+    dbapi_connection.execute("PRAGMA foreign_keys=ON")
+
+AsynSessionFactory = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+async def get_session():
+    session = AsynSessionFactory()
+    try:
+        yield session
+    finally:
+        await session.close()
---
+++ risk_predict/response.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timezone, timedelta
+from pydantic import BaseModel, computed_field
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    created_at: datetime
+
+class LogInResponse(BaseModel):
+    access_token: str
+
+class HealthProfileResponse(BaseModel):
+    id: int
+    user_id: int
+    age: int
+    height_cm: float
+    weight_kg: float
+    smoking: bool
+    exercise_per_week: int
+
+class HealthRiskPredictionResponse(BaseModel):
+    id: int
+    user_id: int
+    diabetes_probability: float
+    hypertension_probability: float
+    # model_version -> 클라이언트에게 공개하지 않을 수 있음
+    created_at: datetime
+    
+    @computed_field
+    @property
+    def created_at_kst(self) -> datetime:
+        KST = timezone(timedelta(hours=9))
+        # UTC 시간을 KST(+09:00)로 변환
+        return self.created_at.astimezone(KST)
