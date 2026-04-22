afffc37d4da1b297b330bb90d6cae7d65d05f435
Dong-hyeon Shin <52447545+qu3vipon@users.noreply.github.com>
2026-04-22 14:53:34 +0900

    Add files via upload

--- risk_predict/auth/password.py
+++ risk_predict/auth/password.py
@@ -1,23 +1,16 @@
 import bcrypt
 
-# bytes = 0 또는 1
 
-# 회원가입 할 때, 비밀번호 해시를 생성하는 함수
 def hash_password(plain_password: str) -> str:
-    password_hash_bytes: bytes = bcrypt.hashpw(
+    password_hash: bytes = bcrypt.hashpw(
         plain_password.encode(), bcrypt.gensalt()
     )
-    return password_hash_bytes.decode()
+    return password_hash.decode()
 
-
-# 로그인 할 때, 비밀번호를 검증하는 함수
-def verify_password(plain_password: str, password_hash: str) -> bool:
-    # plain_password: 사용자가 입력한 평문 비밀번호
-    # password_hash: 데이터베이스에 저장된 비밀번호 해시
-
-    try:
-        return bcrypt.checkpw(
-            plain_password.encode(), password_hash.encode()
-        )
-    except Exception:
-        return False
+def verify_password(
+    plain_password: str, password_hash: str
+) -> bool:
+    return bcrypt.checkpw(
+        plain_password.encode(),
+        password_hash.encode()
+    )
--- risk_predict/main.py
+++ risk_predict/main.py
@@ -1,21 +1,8 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-
-from database.connection import engine
-from database.orm import Base
-
-from routers.user import router as user_router
-from routers.prediction import router as prediction_router
+from user.router import router as user_router
+from prediction.router import router as prediction_router
 
 
-@asynccontextmanager
-async def lifespan(_):
-    # 서버 시작 전, 테이블 자동 생성
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(user_router)
 app.include_router(prediction_router)
